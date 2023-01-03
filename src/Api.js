92dde07c88cd9b9664c612d5bdc7c863d1699dbf
emirjaqem <emirjaqem@gmail.com>
2023-01-03 23:00:58 +0300

    project structure andcreated

---
+++ src/Api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c134805534msh459fecc085985e5p173ef7jsnfcf49471e9e9',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo"
